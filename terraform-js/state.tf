c35b9b2f5d836fd5f2a5438b326492ab9dbe9314
Kyle <kylelatter450@gmail.com>
2025-08-28 17:23:21 -0400

    finished terraform files

---
+++ terraform-js/state.tf
@@ -0,0 +1,8 @@
+terraform {
+    backend "s3" {
+        bucket = "kl-my-tf-website-state"
+        key    = "global/s3/terraform.tfstate"
+        region = "us-east-1"
+        dynamodb_table = "my-db-website-table"
+    }
+}
